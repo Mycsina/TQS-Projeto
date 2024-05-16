0ba4f11e02d4208e967de445d5046730ce91a4f3
Tiago Figueiredo <tiagofigueiredo2003@gmail.com>
2024-05-16 12:28:35 +0100

    feat: add cart skeleton

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,7 +14,7 @@ import RestauranteCard from "@/components/menu/restauranteCard";
 export default function Home() {
     return (
         <div className="grid grid-cols-3 mx-auto w-fit">
-            <div className="h-fit p-5 fixed w-96 bg-slate-100 dark:bg-slate-900 rounded-md shadow-lg">
+            <div className="h-fit p-5 fixed w-96">
                 <h1 className="text-4xl font-semibold mb-2">Restaurants</h1>
                 <Separator />
                 <div className="mt-5 flex flex-col gap-8">
@@ -41,10 +41,10 @@ export default function Home() {
                     </PaginationContent>
                 </Pagination>
             </div>
-            <div className="col-span-2 h-full w-fit col-start-2">
+            <div className="mt-12 col-span-2 h-full w-fit col-start-2">
                 {
                     Array(10).fill(0).map((_, index) => (
-                        <FoodCard key={index} />
+                        <FoodCard key={index} showButton={true} />
                     ))
                 }
             </div>
--- frontend/components/menu/foodCard.tsx
+++ frontend/components/menu/foodCard.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Separator } from '@/components/ui/separator';
 import { Button } from '@/components/ui/button';
 import burger from '@/public/burger.jpeg';
 
-export default function FoodCard() {
+export default function FoodCard(props: { showButton: boolean }) {
     return (
-        <div className="bg-slate-100 dark:bg-slate-900 m-5 flex rounded shadow-lg max-w-3xl">
+        <div className="bg-slate-100 dark:bg-gray-950 m-5 flex rounded shadow-lg max-w-3xl">
             <Image src={burger} alt="Food Item" width={200} height={150} className="aspect-[4/3] rounded shadow-md h-full w-auto" />
             <div className="p-5 flex flex-col">
                 <div className="flex justify-between">
@@ -15,7 +16,11 @@ export default function FoodCard() {
                 <Separator />
                 <p className="mt-5 mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
                 <div className="flex-grow"></div>
-                <Button className='font-semibold'>Add to Cart</Button>
+                {props.showButton &&
+                    <Link href={"/cart"} className={"w-full"}>
+                        <Button className='font-semibold w-full'>Add to Cart</Button>
+                    </Link>
+                }
             </div>
         </div>
     );
