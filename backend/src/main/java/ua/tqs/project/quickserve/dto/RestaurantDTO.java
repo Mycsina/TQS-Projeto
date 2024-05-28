d91c10482fff0da69afd59d132f1dabd668eb530
Alex <71047866+Sytuz@users.noreply.github.com>
2024-05-28 11:07:18 +0100

    Added Restaurant Controller

---
+++ backend/src/main/java/ua/tqs/project/quickserve/dto/RestaurantDTO.java
@@ -0,0 +1,41 @@
+package ua.tqs.project.quickserve.dto;
+
+import java.time.LocalTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import ua.tqs.project.quickserve.entities.Restaurant;
+import ua.tqs.project.quickserve.entities.Address;
+
+@Getter
+@Setter
+public class RestaurantDTO {
+    long restaurantId;
+    String name;
+    String description;
+    int phoneNumber;
+    LocalTime openingTime;
+    LocalTime closingTime;
+    String state;
+    Address address;
+    long menuId;
+    long managerId;
+
+    public RestaurantDTO() {
+    }
+
+    public RestaurantDTO(Restaurant restaurant) {
+        this.restaurantId = restaurant.getId();
+        this.name = restaurant.getName();
+        this.description = restaurant.getDescription();
+        this.phoneNumber = restaurant.getPhone();
+        this.openingTime = restaurant.getOpeningTime();
+        this.closingTime = restaurant.getClosingTime();
+        this.state = restaurant.getState().toString();
+        this.address = restaurant.getAddress();
+        this.menuId = restaurant.getMenu().getId();
+        this.managerId = restaurant.getManager().getId();
+    }
+
+}
--- backend/src/main/java/ua/tqs/project/quickserve/services/RestaurantService.java
+++ backend/src/main/java/ua/tqs/project/quickserve/services/RestaurantService.java
@@ -1,8 +1,11 @@
 package ua.tqs.project.quickserve.services;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ua.tqs.project.quickserve.entities.Restaurant;
+import ua.tqs.project.quickserve.dto.RestaurantDTO;
 import ua.tqs.project.quickserve.repositories.RestaurantRepository;
 
 @Service
@@ -13,6 +16,18 @@ public class RestaurantService {
 
     public Restaurant save(Restaurant restaurant) {
         return repository.save(restaurant);
+    }
+
+    public RestaurantDTO convertRestaurantToDTO(Restaurant restaurant) {
+        return new RestaurantDTO(restaurant);
+    }
+
+    public List<RestaurantDTO> convertRestaurantListToDTOs(List<Restaurant> restaurants) {
+        return restaurants.stream().map(this::convertRestaurantToDTO).toList();
+    }
+
+    public List<Restaurant> getAllRestaurants() {
+        return repository.findAll();
     }
 
     public Restaurant getRestaurantById(long id) {
